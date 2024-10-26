0e682f947ee8571f8d486ef98ed3354d96e51e25
juzruan <justina.ruan@gmail.com>
2024-10-27 00:42:22 +0800

    Update Methodology.py

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -8,20 +8,10 @@ st.set_page_config(
 
 st.sidebar.write("All the info you need about platform work")
 st.sidebar.page_link("Assistant.py", label="Main")
-st.sidebar.page_link("AboutMe.py", label="About Me")
-st.sidebar.page_link("Methodology.py", label="Methodology")
+st.sidebar.page_link("pages/AboutMe.py", label="About Me")
+st.sidebar.page_link("pages/Methodology.py", label="Methodology")
 
 # Add some content to the main app
 st.title("Platform Work Chatbot 💰🤖")
 
-st.write("""
-
-Project Scope: To develop a chatbot that will use the latest published info on Platform Workers Act to draft responses to enquiries
-         
-Objectives: To help confused platform work operators and platform workers find important info about the new Platform Workers Act         
-
-Data sources: MOM website
-
-Features: Real-time scrapping of updated info, scalability and low latency, responsive, fast, natural language understanding and processing          
-         
-""")
+st.image("pages/image.jpg", caption="Methodology", use_column_width=True)
