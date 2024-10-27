35b6407cab527a06e56f9de8ed01801966ca3f78
juzruan <justina.ruan@gmail.com>
2024-10-27 16:19:56 +0800

    Update Contribution_Calculator.py

--- pages/Contribution_Calculator.py
+++ pages/Contribution_Calculator.py
@@ -11,9 +11,9 @@ st.sidebar.write("All the info you need about platform work")
 # Add some content to the main app
 st.title("Platform Work Chatbot 💰🤖")
 
-Age = st.number_input("Your age, as at 1 January of the year, is: ")
+Age = st.number_input("Your age, as at 1 January of the year, is: ", min_value=1,max_value=99,step=1)
 
-GE = st.number_input("Your gross earnings for the year is: $")
+GE = st.number_input("Your gross earnings for the year is: $",min_value=1,step=1)
 
 FEDA = st.radio(
     "What is your mode of transport?",
@@ -21,13 +21,13 @@ FEDA = st.radio(
 
 if FEDA == "Cars, vans, lorries, trucks":
     st.write("Your Fixed Expense Deduction Amount is 60%.")
-    FEDAr == 0.6
+    FEDAr = 0.6
 elif FEDA == "Motorcycles, power-assisted bicycles, motorised personal mobility devices":
     st.write("Your Fixed Expense Deduction Amount is 35%.")
-    FEDAr == 0.35
+    FEDAr = 0.35
 elif FEDA == "Bicycles, on foot (including use of public transport)":
     st.write("Your Fixed Expense Deduction Amount is 20%.")
-    FEDAr == 0.2
+    FEDAr = 0.2
 else:
-    FEDAr == 0
+    FEDAr = 0
 
