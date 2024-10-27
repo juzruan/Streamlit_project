e5e72222e32d20f7426b2fef29adc1aea0229b26
juzruan <justina.ruan@gmail.com>
2024-10-27 16:15:17 +0800

    Update Contribution_Calculator.py

--- pages/Contribution_Calculator.py
+++ pages/Contribution_Calculator.py
@@ -17,7 +17,7 @@ GE = st.number_input("Your gross earnings for the year is: $")
 
 FEDA = st.radio(
     "What is your mode of transport?",
-    ["Cars, vans, lorries, trucks", "Motorcycles, power-assisted bicycles, motorised personal mobility devices", "Bicycles, on foot (including use of public transport)","None"],
+    ["Cars, vans, lorries, trucks", "Motorcycles, power-assisted bicycles, motorised personal mobility devices", "Bicycles, on foot (including use of public transport)","None"])
 
 if FEDA == "Cars, vans, lorries, trucks":
     st.write("Your Fixed Expense Deduction Amount is 60%.")
@@ -31,5 +31,5 @@ else:
     st.write("Your Fixed Expense Deduction Amount is 20%.")
     FEDAr == 0.2
 else:
-    FEDAr == 0)
+    FEDAr == 0
 
